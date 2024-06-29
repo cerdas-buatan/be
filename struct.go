2375006aad8a0518afc96e0f2d8be45c5cbe0612
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-06-29 11:13:51 +0700

    sabtu-membuat struct register

---
+++ struct.go
@@ -0,0 +1,15 @@
+package be
+
+import (
+    "fmt"
+)
+
+type Register struct {
+    Email           string
+    Username        string
+    Password        string
+    ConfirmPassword string
+}
+
+
+
