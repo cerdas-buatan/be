7e3d828cc3c7d2459d165be576881c1129e65997
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-10 17:31:56 +0700

    commit-edit

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
-package main
+package be
 
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/cerdas-buatan/be/config"
 	"github.com/cerdas-buatan/be/route"
 	"os"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
+
+func init() {
+	functions.HTTP("makmur", route.Web)
+}
 
 func main() {
 	app := fiber.New()
