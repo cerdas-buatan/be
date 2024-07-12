f948dbaed283dd8c69db042e29584603c9ff27cc
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-12 17:30:04 +0700

    hapus func

--- main.go
+++ main.go
@@ -17,13 +17,10 @@ func init() {
 // 	app.Listen(":3000")
 // }
 
-// package main
-
 // import (
 //     "net/http"
 //     "github.com/cerdas-buatan/be/module"
 // )
-
 // func main() {
 //     http.HandleFunc("/chat", handler.ChatHandler)
 //     http.ListenAndServe(":8080", nil)
