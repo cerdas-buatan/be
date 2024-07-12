b64b4190890c926a7b8baddfd3fbaf4c8573d4e8
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-12 17:30:33 +0700

    update main lagi

--- main.go
+++ main.go
@@ -17,10 +17,13 @@ func init() {
 // 	app.Listen(":3000")
 // }
 
+// package main
+
 // import (
 //     "net/http"
 //     "github.com/cerdas-buatan/be/module"
 // )
+
 // func main() {
 //     http.HandleFunc("/chat", handler.ChatHandler)
 //     http.ListenAndServe(":8080", nil)
