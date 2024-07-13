9ca5ab4dbfc308dab553db0cd8e455f8adb7fb02
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-13 20:07:46 +0700

    rapihin dan hapus yg tidak perlu

--- helper/respons.go
+++ helper/respons.go
@@ -16,43 +16,15 @@ func Jsonstr(strc interface{}) string {
     return string(jsonData)
 }
 
-
-
-
-// func GCFReturnStruct(DataStruct interface{}) string {
-// 	jsonData, _ := json.Marshal(DataStruct)
-// 	return string(jsonData)
-// }
-
-
-
-
-
-
-
-
-
-
-
-// package main
-// import(
-// 	"net/http"
-// 	"github.com/gofiber/fiber/v2/log"
-// 	"encoding/json"
-// )
-
 // func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
 //     respw.Header().Set("Content-Type", "application/json")
 //     respw.WriteHeader(statusCode)
 //     respw.Write([]byte(Jsonstr(content)))
 // }
 
-// func Jsonstr(strc interface{}) string {
-//     jsonData, err := json.Marshal(strc)
-//     if (err != nil) {
-//         log.Fatal(err)
-//     }
-//     return string(jsonData)
+// func GCFReturnStruct(DataStruct interface{}) string {
+// 	jsonData, _ := json.Marshal(DataStruct)
+// 	return string(jsonData)
 // }
 
 // func SendResponse(c *fiber.Ctx, statusCode int, message string, data interface{}) error {
