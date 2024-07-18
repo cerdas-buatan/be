7c7650ac23689c8768047ce535dff2c3f7453c03
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-18 20:34:01 +0700

    add type

--- model/type.go
+++ model/type.go
@@ -126,3 +126,10 @@ type Menu struct {
 // 	Question string `json:"question"`
 // 	Answer   string `json:"answer"`
 // }
+//type Credential struct {
+// 	Status  bool   `json:"status" bson:"status"`
+// 	Token   string `json:"token,omitempty" bson:"token,omitempty"`
+// 	Message string `json:"message,omitempty" bson:"message,omitempty"`
+// 	Data    []User `json:"data,omitempty" bson:"data,omitempty"`
+// 	Role    string `json:"role,omitempty" bson:"role,omitempty"`
+// }
