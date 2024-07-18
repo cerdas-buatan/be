b524f898f481f8d7e3dc45e2e1b569a1383aa89e
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-18 20:39:12 +0700

    nambah struct chat

--- type.go
+++ type.go
@@ -19,3 +19,16 @@ type Pengguna struct {
 	Username string             `bson:"username,omitempty" json:"username,omitempty"`  
 	Akun     User               `bson:"akun,omitempty" json:"akun,omitempty"`  
 }
+
+// struct response
+type Response struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+// struct ChatRequest
+type ChatRequest struct {
+	Message  string `json:"message"`
+	Response string `json:"response"`
+}
