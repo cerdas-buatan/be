3b7b3ae454dc7e880fb266be0bc54d2eead911c2
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-18 20:16:08 +0700

    update func respon

--- helper/respons.go
+++ helper/respons.go
@@ -1,19 +1,25 @@
 package helper
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
-    respw.Header().Set("Content-Type", "application/json")
-    respw.WriteHeader(statusCode)
-    respw.Write([]byte(Jsonstr(content)))
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(Jsonstr(content)))
 }
 
 func Jsonstr(strc interface{}) string {
-    jsonData, err := json.Marshal(strc)
-    if (err != nil) {
-        log.Fatal(err)
-    }
-    return string(jsonData)
+	jsonData, err := json.Marshal(strc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonData)
 }
 
 // func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
@@ -27,9 +33,9 @@ func Jsonstr(strc interface{}) string {
 // 	return string(jsonData)
 // }
 
-// func SendResponse(c *fiber.Ctx, statusCode int, message string, data interface{}) error {
-// 	return c.Status(statusCode).JSON(fiber.Map{
-// 		"message": message,
-// 		"data":    data,
-// 	})
-// }
+func SendResponse(c *fiber.Ctx, statusCode int, message string, data interface{}) error {
+	return c.Status(statusCode).JSON(fiber.Map{
+		"message": message,
+		"data":    data,
+	})
+}
