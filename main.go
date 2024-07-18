e5725904363d21b83aa1f29061b1d4685b579546
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-18 19:51:35 +0700

    update main

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package be
 
 import (
-	"encoding/json"
-	"net/http"
-	"content"
-	"log"
-	"os"
-
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	route "github.com/cerdas-buatan/be/route"
-	"github.com/whatsauth/watoken"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func init() {
 	functions.HTTP("gaysdisal", route.Web)
 }
-
-
 
 // func HomeGaysdisal(w http.ResponseWriter, r *http.Request) {
 // 	w.Header().Set("Content-Type", "application/json")
