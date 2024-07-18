88606a8e1324e3abeda1a0ca1f95b252dba3982e
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-18 20:42:36 +0700

    sambungin db

---
+++ mongo.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// connection db
+func ConnectDB(uri string) *mongo.Database {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client.Database(dbName)
+}
