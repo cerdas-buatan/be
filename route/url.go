c9e774ca126dd4e4c744130df9c8f2f95fdc4aeb
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-18 23:45:22 +0700

    updated url

--- route/url.go
+++ route/url.go
@@ -31,9 +31,20 @@ func SetupRouter(app *fiber.App, database *mongo.Database) {
 }
 
 func Web(w http.ResponseWriter, r *http.Request) {
-	app := fiber.New()
-	SetupRouter(app, db)
-	app.Handler()(w, r)
+	if config.SetAccessControlHeaders(w, r) {
+		return // If it's a preflight request, return early.
+	}
+	var method, path string = r.Method, r.URL.Path
+	switch {
+	case method == "GET" && path == "/":
+		module.HomeGaysdisal(w, r)
+	case method == "POST" && path == "/registerai":
+		module.RegisterUsers(w, r)
+	case method == "POST" && path == "/loginai":
+		module.LoginUsers(w, r)
+	default:
+		module.NotFound(w, r)
+	}
 }
 
 // func Web(w http.ResponseWriter, r *http.Request) {
