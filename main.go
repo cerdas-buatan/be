3afe1c81e927fb769ed032fbb5b9e935342b7906
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-20 15:22:35 +0700

    update main

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package be
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	route "github.com/cerdas-buatan/be/route"
 )
@@ -9,10 +12,10 @@ func init() {
 	functions.HTTP("gaysdisal", route.Web)
 }
 
-// func HomeGaysdisal(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(map[string]interface{}{
-// 		"message": "Welcome to Gaysdisal",
-// 	})
-// }
+func HomeGaysdisal(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Welcome to Gaysdisal",
+	})
+}
