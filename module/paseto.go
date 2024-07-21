575a0c2509297c86b92e80c7bc5672c895e17a0f
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-21 23:08:25 +0700

    delete unused func in paseto.go

--- module/paseto.go
+++ module/paseto.go
@@ -49,23 +49,6 @@ func Decode(secret, token string) (model.Payload, error) {
 // 	return token.V4Sign(secretKey, nil), err
 // }
 
-// func Decode2(publicKey string, tokenstring string) (payload model.Payload, err error) {
-// 	var token *paseto.Token
-// 	var pubKey paseto.V4AsymmetricPublicKey
-// 	pubKey, err = paseto.NewV4AsymmetricPublicKeyFromHex(publicKey) // this wil fail if given key in an invalid format
-// 	if err != nil {
-// 		fmt.Println("Decode NewV4AsymmetricPublicKeyFromHex : ", err)
-// 	}
-// 	parser := paseto.NewParser()                                // only used because this example token has expired, use NewParser() (which checks expiry by default)
-// 	token, err = parser.ParseV4Public(pubKey, tokenstring, nil) // this will fail if parsing failes, cryptographic checks fail, or validation rules fail
-// 	if err != nil {
-// 		fmt.Println("Decode ParseV4Public : ", err)
-// 	} else {
-// 		json.Unmarshal(token.ClaimsJSON(), &payload)
-// 	}
-// 	return payload, err
-// }
-
 func Encode(id primitive.ObjectID, role, privateKey string) (string, error) {
 	token := paseto.NewToken()
 	token.SetIssuedAt(time.Now())
