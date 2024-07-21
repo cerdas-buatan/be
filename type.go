34fe247c3056ff17e4b4d0a462abad7576d76e58
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-21 22:31:25 +0700

    update chat respon

--- type.go
+++ type.go
@@ -41,10 +41,10 @@ type Credential struct {
 }
 
 type ChatResponse struct {
-	ResponseID primitive.ObjectID `bson:"response_id,omitempty" json:"response_id,omitempty"`
+//	ResponseID primitive.ObjectID `bson:"response_id,omitempty" json:"response_id,omitempty"`
 	Message    string             `json:"message"`   
-	Timestamp  int64              `json:"timestamp"` 
-	UserID     primitive.ObjectID `bson:"user_id,omitempty" json:"user_id,omitempty"`
+//	Timestamp  int64              `json:"timestamp"` 
+//	UserID     primitive.ObjectID `bson:"user_id,omitempty" json:"user_id,omitempty"`
 }
 
 // struct LoginRequest
