800c850e08d165a34502e4ffeac69f49c6bab8d8
Ardiva <Ardiva.diva30@gmail.com>
2024-07-21 23:30:42 +0700

    perbaiki func di menu.go

--- module/menu.go
+++ module/menu.go
@@ -1,5 +1,3 @@
-// module/menu.go
-
 package module
 
 import (
@@ -13,9 +11,11 @@ import (
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
+
 type MenuService struct {
     collection *mongo.Collection
 }
+
 
 func NewMenuService(db *mongo.Database) *MenuService {
     return &MenuService{
@@ -23,11 +23,13 @@ func NewMenuService(db *mongo.Database) *MenuService {
     }
 }
 
+
 func (s *MenuService) CreateMenu(ctx context.Context, menu model.Menu) (model.Menu, error) {
     menu.ID = primitive.NewObjectID()
     _, err := s.collection.InsertOne(ctx, menu)
     return menu, err
 }
+
 
 func (s *MenuService) GetMenu(ctx context.Context, id primitive.ObjectID) (model.Menu, error) {
     var menu model.Menu
@@ -35,15 +37,18 @@ func (s *MenuService) GetMenu(ctx context.Context, id primitive.ObjectID) (model
     return menu, err
 }
 
+
 func (s *MenuService) UpdateMenu(ctx context.Context, id primitive.ObjectID, menu model.Menu) error {
     _, err := s.collection.UpdateOne(ctx, bson.M{"_id": id}, bson.M{"$set": menu})
     return err
 }
 
+
 func (s *MenuService) DeleteMenu(ctx context.Context, id primitive.ObjectID) error {
     _, err := s.collection.DeleteOne(ctx, bson.M{"_id": id})
     return err
 }
+
 
 func (s *MenuService) ListMenus(ctx context.Context) ([]model.Menu, error) {
     var menus []model.Menu
