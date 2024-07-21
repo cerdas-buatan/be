af3da36a9ea933b5a5182739a2722997c338e3fd
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 21:19:21 +0700

    delete unused func

--- mongo.go
+++ mongo.go
@@ -189,11 +189,3 @@ func InsertUserdata(database *mongo.Database, username, email, password, salt, r
 	return InsertTwoDoc(database, "users", pengguna)
 }
 
-func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (interface{}, error) {
-	coll := db.Collection(collection)
-	result, err := coll.InsertOne(context.Background(), doc)
-	if err != nil {
-		return nil, err
-	}
-	return result.InsertedID, nil
-}
