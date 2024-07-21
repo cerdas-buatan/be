ff22712e91502ac30c266354b59ce4aee6a5788d
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 21:20:17 +0700

    delete unused func

--- mongo.go
+++ mongo.go
@@ -77,22 +77,3 @@ func GCFReturnStruct(DataStuct any) string {
 	return string(jsondata)
 }
 
-// InsertUserdata insert user untuk register
-func InsertUserdata(database *mongo.Database, username, email, password, salt, role string) (InsertedID interface{}) {
-	// Create the User struct
-	user := model.User{
-		Email:    email,
-		Password: password,
-		Salt:     salt,
-		Role:     role,
-	}
-
-	// Create the Pengguna struct
-	pengguna := model.Pengguna{
-		Username: username,
-		Akun:     user,
-	}
-
-	return InsertTwoDoc(database, "users", pengguna)
-}
-
