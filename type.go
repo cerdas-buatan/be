a10c8b4a6e7270d8fd35afa838905d16e315a0a9
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 20:59:16 +0700

    add more struct

--- type.go
+++ type.go
@@ -59,3 +59,12 @@ type Pengguna struct {
 	Username string             `bson:"username,omitempty" json:"username,omitempty"`  
 	Akun     User               `bson:"akun,omitempty" json:"akun,omitempty"`  
 }
+
+// struct user
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Email    string             `bson:"email,omitempty" json:"email,omitempty"`
+	Password string             `bson:"password,omitempty" json:"password,omitempty"`
+	Salt     string             `bson:"salt,omitempty" json:"salt,omitempty"`
+	Role     string             `bson:"role,omitempty" json:"role,omitempty"`
+}
