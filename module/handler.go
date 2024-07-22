7a2f0b7f283c9bf3a8fcb96d0146f51f14365704
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-22 22:40:04 +0700

    modifikasi function signup

--- module/handler.go
+++ module/handler.go
@@ -15,75 +15,52 @@ import (
 
 
 // GCFHandlerSignUpPengguna handles signup for Google Cloud Function
-func GCFSignUp(w http.ResponseWriter, r *http.Request) {
-	db := helper.ConnectMongoDB(os.Getenv("MONGOCONNSTRING"), os.Getenv("DBNAME"))
-	defer db.Client().Disconnect(r.Context())
-
+func GCFHandlerSignUpPengguna(MONGOCONNSTRINGENV, dbname string, r *http.Request) string {
+	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
 	var Response model.Response
 	Response.Status = false
-
-	var userdata model.Pengguna
-	err := json.NewDecoder(r.Body).Decode(&userdata)
-	if err != nil {
-		Response.Message = "error parsing application/json: " + err.Error()
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(Response)
-		return
-	}
-
-	err = helper.SignUpPengguna(db, userdata)
-	if err != nil {
-		Response.Message = err.Error()
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(Response)
-		return
-	}
-
-	Response.Status = true
-	Response.Message = "Halo, Selamat Bergabung " + userdata.Username
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Response)
-}
-
-
-// GCFHandlerLogin handles login for Google Cloud Function
-func GCFSignIn(w http.ResponseWriter, r *http.Request) {
-	db := helper.ConnectMongoDB(os.Getenv("MONGOCONNSTRING"), os.Getenv("DBNAME"))
-	defer db.Client().Disconnect(r.Context())
-
-	var Response model.Credential
-	Response.Status = false
-
-	var datapengguna model.User
+	var datapengguna model.Pengguna
 	err := json.NewDecoder(r.Body).Decode(&datapengguna)
 	if err != nil {
 		Response.Message = "error parsing application/json: " + err.Error()
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(Response)
-		return
+		return GCFReturnStruct(Response)
 	}
-
-	user, err := helper.LogIn(db, datapengguna)
+	err = SignUpPengguna(conn, datapengguna)
 	if err != nil {
 		Response.Message = err.Error()
-		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(Response)
-		return
+		return GCFReturnStruct(Response)
 	}
-
 	Response.Status = true
-	tokenstring, err := helper.Encode(user.ID, user.Role, os.Getenv("PASETOPRIVATEKEYENV"))
+	Response.Message = "Halo " + datapengguna.Username
+	return GCFReturnStruct(Response)
+}
+
+// RegisterHandler 
+func Register(MONGOCONNSTRINGENV, dbname string, r *http.Request) string {
+	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var Response model.Response
+	Response.Status = false
+	var datapengguna model.Pengguna
+	err := json.NewDecoder(r.Body).Decode(&datapengguna)
 	if err != nil {
-		Response.Message = "Gagal Encode Token : " + err.Error()
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		Response.Message = "Halo, Selamat Datang " + user.Email
-		Response.Token = tokenstring
-		Response.Role = user.Role
+		Response.Message = "error parsing application/json: " + err.Error()
+		return GCFReturnStruct(Response)
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Response)
+	// Validasi email sebelum proses pendaftaran
+	if !strings.Contains(datapengguna.Akun.Email, "@") {
+		Response.Message = "Email tidak benar karena tidaka ada simbol '@'"
+		return GCFReturnStruct(Response)
+	}
+
+	err = SignUpPengguna(conn, datapengguna)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Halo " + datapengguna.Username
+	return GCFReturnStruct(Response)
 }
 
 // GetResponse generates a response based on the input message
@@ -99,33 +76,6 @@ func GCFGetResponse(message string, db *mongo.Database) (string, error) {
 	}
 
 	return Responsechat.Response, nil
-}
-
-// ChatHandler handles chat requests
-func GCFChat(MongoString, dbname string, w http.ResponseWriter, r *http.Request) {
-	var Reqchat model.ChatRequest
-	err := json.NewDecoder(r.Body).Decode(&Reqchat)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	db := helper.MongoConnect(MongoString, dbname)
-	defer func() {
-		if err := db.Client().Disconnect(context.TODO()); err != nil {
-			log.Printf("Failed to disconnect from MongoDB: %v", err)
-		}
-	}()
-
-	response, err := GetResponse(Reqchat.Message, db) 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	Reqchat := model.ChatResponse{Response: response}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Reqchat)
 }
 
 
