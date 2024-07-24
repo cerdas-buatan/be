485d9744588eb982325a5d9f4fdda60d4412b339
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 19:15:54 +0700

    func NewMenuService

--- menuoth.go
+++ menuoth.go
@@ -10,3 +10,14 @@ import (
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type MenuService struct {
+    collection *mongo.Collection
+}
+
+
+func NewMenuService(db *mongo.Database) *MenuService {
+    return &MenuService{
+        collection: db.Collection("menus"),
+    }
+}
