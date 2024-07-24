07e4f5f17328c41573296188dcf148be7a60284a
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 19:18:11 +0700

    delete func unused

--- menuoth.go
+++ menuoth.go
@@ -27,10 +27,3 @@ func (s *MenuService) CreateMenu(ctx context.Context, menu model.Menu) (model.Me
     _, err := s.collection.InsertOne(ctx, menu)
     return menu, err
 }
-
-func (s *MenuService) GetMenu(ctx context.Context, id primitive.ObjectID) (model.Menu, error) {
-    var menu model.Menu
-    err := s.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&menu)
-    return menu, err
-}
-
