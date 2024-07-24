1bb568bf4b97e7445321d2d162fc20ed4ea72713
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 19:19:15 +0700

    delete func unused

--- menuoth.go
+++ menuoth.go
@@ -110,25 +110,3 @@ func GCFHandlerSignIn(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname string, r 
 	}
 	return GCFReturnStruct(Response)
 }
-
-func SignIn(db *mongo.Database, insertedDoc model.User) (user model.User, err error) {
-	if insertedDoc.Email == "" || insertedDoc.Password == "" {
-		return user, fmt.Errorf("mohon untuk melengkapi data")
-	}
-	if err = checkmail.ValidateFormat(insertedDoc.Email); err != nil {
-		return user, fmt.Errorf("email tidak valid")
-	}
-	existsDoc, err := GetUserFromEmail(insertedDoc.Email, db)
-	if err != nil {
-		return
-	}
-	salt, err := hex.DecodeString(existsDoc.Salt)
-	if err != nil {
-		return user, fmt.Errorf("kesalahan server : salt")
-	}
-	hash := argon2.IDKey([]byte(insertedDoc.Password), salt, 1, 64*1024, 4, 32)
-	if hex.EncodeToString(hash) != existsDoc.Password {
-		return user, fmt.Errorf("password salah")
-	}
-	return existsDoc, nil
-}
