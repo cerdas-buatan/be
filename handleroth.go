510d5be52fd48a47a47861c0761b6dd16c11cd92
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 19:12:19 +0700

    GCFHandlerSignUpPengguna

--- handleroth.go
+++ handleroth.go
@@ -12,3 +12,24 @@ import (
 	"github.com/whatsauth/watoken"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+// GCFHandlerSignUpPengguna handles signup for Google Cloud Function
+func GCFHandlerSignUp(MONGOCONNSTRINGENV, dbname string, r *http.Request) string {
+	conn := MongoConnect(MONGOCONNSTRINGENV, dbname)
+	var Response model.Response
+	Response.Status = false
+	var datauser model.Pengguna
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+		return GCFReturnStruct(Response)
+	}
+	err = SignUp(conn, datauser)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Halo, Selamat Bergabung" + datauser.Username
+	return GCFReturnStruct(Response)
+}
