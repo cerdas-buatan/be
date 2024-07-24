497ebc41f9d3d324df02bbf67abb015a93534ccd
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 19:18:37 +0700

    delete func unused

--- menuoth.go
+++ menuoth.go
@@ -1,12 +1,2 @@
 package module
 
-import (
-    "context"
-    "time"
-
-    "github.com/cerdas-buatan/be/model"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-)
