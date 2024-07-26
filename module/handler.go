deae6978eeed213b97b17e31a757912d39d21ef8
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-26 15:46:44 +0700

    updated new func

--- module/handler.go
+++ module/handler.go
@@ -34,8 +34,85 @@ func GCFHandlerSignUp(Mongoenv, dbname string, r *http.Request) string {
 	return GCFReturnStruct(Response)
 }
 
+func GCFHandlerSignUp2(Mongoenv, dbname string, r *http.Request) string {
+	conn := MongoConnect(Mongoenv, dbname)
+	var Response model.Response
+	Response.Status = false
+	var datauser model.Pengguna
+	var code string
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+		return GCFReturnStruct(Response)
+	}
+
+	// Extract verification code if provided
+	code = r.URL.Query().Get("code")
+	
+	err = SignUp(conn, datauser, code)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Halo, Selamat Bergabung " + datauser.Username
+	return GCFReturnStruct(Response)
+}
+
 
 // signup
+func SignUp2(db *mongo.Database, insertedDoc model.Pengguna, code string) error {
+	objectId := primitive.NewObjectID()
+	if insertedDoc.Username == "" ||
+		insertedDoc.Akun.Password == "" {
+		return fmt.Errorf("dimohon untuk melengkapi data")
+	}
+	if err := checkmail.ValidateFormat(insertedDoc.Akun.Email); err != nil {
+		return fmt.Errorf("email tidak valid")
+	}
+	userExists, _ := GetUserFromEmail(insertedDoc.Akun.Email, db)
+	if insertedDoc.Akun.Email == userExists.Email {
+		return fmt.Errorf("email sudah terdaftar")
+	}
+	if strings.Contains(insertedDoc.Akun.Password, " ") {
+		return fmt.Errorf("password tidak boleh mengandung spasi")
+	}
+	if len(insertedDoc.Akun.Password) < 8 {
+		return fmt.Errorf("password terlalu pendek")
+	}
+	if !ValidateVerificationCode(insertedDoc.Akun.PhoneNumber, code) {
+		return fmt.Errorf("kode verifikasi tidak valid")
+	}
+
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return fmt.Errorf("kesalahan server : salt")
+	}
+	hashedPassword := argon2.IDKey([]byte(insertedDoc.Akun.Password), salt, 1, 64*1024, 4, 32)
+	user := bson.M{
+		"_id":      objectId,
+		"email":    insertedDoc.Akun.Email,
+		"password": hex.EncodeToString(hashedPassword),
+		"salt":     hex.EncodeToString(salt),
+	}
+	pengguna := bson.M{
+		"username": insertedDoc.Username,
+		"akun": model.User{
+			ID: objectId,
+		},
+	}
+	_, err = InsertOneDoc(db, "user", user)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+	_, err = InsertOneDoc(db, "pengguna", pengguna)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+	return nil
+}
+
 // signup
 func SignUp(db *mongo.Database, insertedDoc model.Pengguna) error {
 	objectId := primitive.NewObjectID()
