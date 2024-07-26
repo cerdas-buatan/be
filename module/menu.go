cfa220ffb3de3493100ad2098a91b26d93e45f0a
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-26 15:56:46 +0700

    updated

--- module/menu.go
+++ module/menu.go
@@ -2,20 +2,16 @@ package module
 
 import (
     "context"
-    "time"
-
-    "github.com/cerdas-buatan/be/model"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
+    "github.com/cerdas-buatan/be/model"
 )
-
 
 type MenuService struct {
     collection *mongo.Collection
 }
-
 
 func NewMenuService(db *mongo.Database) *MenuService {
     return &MenuService{
@@ -23,33 +19,34 @@ func NewMenuService(db *mongo.Database) *MenuService {
     }
 }
 
-
+// CreateMenu creates a new menu item and stores it in the database
 func (s *MenuService) CreateMenu(ctx context.Context, menu model.Menu) (model.Menu, error) {
     menu.ID = primitive.NewObjectID()
     _, err := s.collection.InsertOne(ctx, menu)
     return menu, err
 }
 
-
+// GetMenu retrieves a menu item by its ID
 func (s *MenuService) GetMenu(ctx context.Context, id primitive.ObjectID) (model.Menu, error) {
     var menu model.Menu
     err := s.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&menu)
     return menu, err
 }
 
-
-func (s *MenuService) UpdateMenu(ctx context.Context, id primitive.ObjectID, menu model.Menu) error {
-    _, err := s.collection.UpdateOne(ctx, bson.M{"_id": id}, bson.M{"$set": menu})
+// UpdateMenu updates the details of an existing menu item
+func (s *MenuService) UpdateMenu(ctx context.Context, id primitive.ObjectID, updateFields map[string]interface{}) error {
+    update := bson.M{"$set": updateFields}
+    _, err := s.collection.UpdateOne(ctx, bson.M{"_id": id}, update)
     return err
 }
 
-
+// DeleteMenu removes a menu item from the database
 func (s *MenuService) DeleteMenu(ctx context.Context, id primitive.ObjectID) error {
     _, err := s.collection.DeleteOne(ctx, bson.M{"_id": id})
     return err
 }
 
-
+// ListMenus retrieves all menu items from the database
 func (s *MenuService) ListMenus(ctx context.Context) ([]model.Menu, error) {
     var menus []model.Menu
     cursor, err := s.collection.Find(ctx, bson.M{})
@@ -65,4 +62,24 @@ func (s *MenuService) ListMenus(ctx context.Context) ([]model.Menu, error) {
         menus = append(menus, menu)
     }
     return menus, nil
+}
+
+// AddChatHistory creates a new chat history entry
+func (s *MenuService) AddChatHistory(ctx context.Context, chat model.ChatHistory) (model.ChatHistory, error) {
+    chat.ID = primitive.NewObjectID()
+    _, err := s.collection.InsertOne(ctx, chat)
+    return chat, err
+}
+
+// DeleteChatHistory removes a chat history entry by its ID
+func (s *MenuService) DeleteChatHistory(ctx context.Context, id primitive.ObjectID) error {
+    _, err := s.collection.DeleteOne(ctx, bson.M{"_id": id})
+    return err
+}
+
+// UpdateChatHistory updates a specific chat history entry
+func (s *MenuService) UpdateChatHistory(ctx context.Context, id primitive.ObjectID, updateFields map[string]interface{}) error {
+    update := bson.M{"$set": updateFields}
+    _, err := s.collection.UpdateOne(ctx, bson.M{"_id": id}, update)
+    return err
 }
