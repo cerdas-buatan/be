c34394268f0933a9d4bb11215a554ce3f2d45fce
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-28 19:19:46 +0700

    modify

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"log"
+	"net/http"
 
+	"github.com/cerdas-buatan/be/config"
+	"github.com/cerdas-buatan/be/module"
+	"github.com/cerdas-buatan/be/route"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	route "github.com/cerdas-buatan/be/route"
 )
@@ -13,6 +18,18 @@ import (
 
 func init() {
 	functions.HTTP("gaysdisal", route.Web)
+}
+
+func main() {
+	// Initialize database connection and services
+	db := config.InitDB()
+	menuService := module.NewMenuService(db)
+
+	// Initialize routes
+	url.InitRoutes(menuService)
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
