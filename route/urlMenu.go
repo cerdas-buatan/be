fde6365e81bd43f9edbfcc99409488a0604515f1
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-28 11:19:32 +0700

    updated

--- route/urlMenu.go
+++ route/urlMenu.go
@@ -1,12 +1,11 @@
 package route
 import (
-	"github.com/kimseokgis/backend-ai/controller"
-	"github.com/kimseokgis/backend-ai/module"
+	"github.com/cerdas-buatan/be/module"
 	"net/http"
 )
 
 func MenuRoutes(menuService *module.MenuService) {
-	http.HandleFunc("/menu/rename", controller.RenameMenuHandler(menuService))
-	http.HandleFunc("/menu/archive", controller.ArchiveMenuHandler(menuService))
-	http.HandleFunc("/menu/add", controller.AddMenuHandler(menuService))
+	http.HandleFunc("/menu/rename", module.RenameMenuHandler(menuService))
+	http.HandleFunc("/menu/archive", module.ArchiveMenuHandler(menuService))
+	http.HandleFunc("/menu/add", module.AddMenuHandler(menuService))
 }
