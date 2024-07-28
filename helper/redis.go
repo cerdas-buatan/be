83a0305eda21718375838e24dc039bb1419a691d
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-28 11:06:22 +0700

    updated function again

--- helper/redis.go
+++ helper/redis.go
@@ -2,11 +2,15 @@ package helper
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
+	"github.com/whatsauth/whatsauth"
 	"os"
+	"time"
 )
 
 var rdb *redis.Client
+var waClient *whatsauth.Client
 
 func InitRedisClient() {
 	rdb = redis.NewClient(&redis.Options{
@@ -14,6 +18,10 @@ func InitRedisClient() {
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
+}
+
+func InitWhatsAuthClient() {
+	waClient = whatsauth.NewClient(os.Getenv("WHATSAPP_API_KEY"))
 }
 
 func SetVerificationCode(phoneNumber, code string) error {
@@ -24,4 +32,14 @@ func SetVerificationCode(phoneNumber, code string) error {
 func GetVerificationCode(phoneNumber string) (string, error) {
 	ctx := context.Background()
 	return rdb.Get(ctx, phoneNumber).Result()
+}
+
+func SendWhatsAppMessage(phoneNumber, code string) error {
+	message := fmt.Sprintf("Your verification code is %s", code)
+	waToken, err := waClient.Send(phoneNumber, message)
+	if err != nil {
+		return fmt.Errorf("error sending WhatsApp message: %v", err)
+	}
+	fmt.Printf("Sent WhatsApp message to %s with code %s\n", phoneNumber, code)
+	return waToken.Send()
 }
--- module/handler.go
+++ module/handler.go
@@ -35,86 +35,6 @@ func GCFHandlerSignUp(Mongoenv, dbname string, r *http.Request) string {
 	Response.Message = "Halo, Selamat Bergabung" + datauser.Username
 	return GCFReturnStruct(Response)
 }
-
-func GCFHandlerSignUp2(Mongoenv, dbname string, r *http.Request) string {
-	conn := MongoConnect(Mongoenv, dbname)
-	var Response model.Response
-	Response.Status = false
-	var datauser model.Pengguna
-	var code string
-	err := json.NewDecoder(r.Body).Decode(&datauser)
-	if err != nil {
-		Response.Message = "error parsing application/json: " + err.Error()
-		return GCFReturnStruct(Response)
-	}
-
-	// Extract verification code if provided
-	code = r.URL.Query().Get("code")
-	
-	err = SignUp(conn, datauser, code)
-	if err != nil {
-		Response.Message = err.Error()
-		return GCFReturnStruct(Response)
-	}
-	Response.Status = true
-	Response.Message = "Halo, Selamat Bergabung " + datauser.Username
-	return GCFReturnStruct(Response)
-}
-
-
-// signup
-func SignUp2(db *mongo.Database, insertedDoc model.Pengguna, code string) error {
-	objectId := primitive.NewObjectID()
-	if insertedDoc.Username == "" ||
-		insertedDoc.Akun.Password == "" {
-		return fmt.Errorf("dimohon untuk melengkapi data")
-	}
-	if err := checkmail.ValidateFormat(insertedDoc.Akun.Email); err != nil {
-		return fmt.Errorf("email tidak valid")
-	}
-	userExists, _ := GetUserFromEmail(insertedDoc.Akun.Email, db)
-	if insertedDoc.Akun.Email == userExists.Email {
-		return fmt.Errorf("email sudah terdaftar")
-	}
-	if strings.Contains(insertedDoc.Akun.Password, " ") {
-		return fmt.Errorf("password tidak boleh mengandung spasi")
-	}
-	if len(insertedDoc.Akun.Password) < 8 {
-		return fmt.Errorf("password terlalu pendek")
-	}
-	if !ValidateVerificationCode(insertedDoc.Akun.PhoneNumber, code) {
-		return fmt.Errorf("kode verifikasi tidak valid")
-	}
-
-	salt := make([]byte, 16)
-	_, err := rand.Read(salt)
-	if err != nil {
-		return fmt.Errorf("kesalahan server : salt")
-	}
-	hashedPassword := argon2.IDKey([]byte(insertedDoc.Akun.Password), salt, 1, 64*1024, 4, 32)
-	user := bson.M{
-		"_id":      objectId,
-		"email":    insertedDoc.Akun.Email,
-		"password": hex.EncodeToString(hashedPassword),
-		"salt":     hex.EncodeToString(salt),
-	}
-	pengguna := bson.M{
-		"username": insertedDoc.Username,
-		"akun": model.User{
-			ID: objectId,
-		},
-	}
-	_, err = InsertOneDoc(db, "user", user)
-	if err != nil {
-		return fmt.Errorf("kesalahan server")
-	}
-	_, err = InsertOneDoc(db, "pengguna", pengguna)
-	if err != nil {
-		return fmt.Errorf("kesalahan server")
-	}
-	return nil
-}
-
 // signup
 func SignUp(db *mongo.Database, insertedDoc model.Pengguna) error {
 	objectId := primitive.NewObjectID()
@@ -163,6 +83,91 @@ func SignUp(db *mongo.Database, insertedDoc model.Pengguna) error {
 	}
 	return nil
 }
+
+// GCFHandlerSignUpPengguna handles signup for Google Cloud Function
+func GCFHandlerSignUp2(Mongoenv, dbname string, r *http.Request) string {
+	conn := MongoConnect(Mongoenv, dbname)
+	var Response model.Response
+	Response.Status = false
+	var datauser model.Pengguna
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+		return GCFReturnStruct(Response)
+	}
+	err = SignUp(conn, datauser)
+	if err != nil {
+		Response.Message = err.Error()
+		return GCFReturnStruct(Response)
+	}
+	Response.Status = true
+	Response.Message = "Halo, Selamat Bergabung " + datauser.Username
+	return GCFReturnStruct(Response)
+}
+
+// SignUp handles the logic for signing up a user
+func SignUp2(db *mongo.Database, insertedDoc model.Pengguna) error {
+	objectId := primitive.NewObjectID()
+	if insertedDoc.Username == "" ||
+		insertedDoc.Akun.Password == "" {
+		return fmt.Errorf("dimohon untuk melengkapi data")
+	}
+	if err := checkmail.ValidateFormat(insertedDoc.Akun.Email); err != nil {
+		return fmt.Errorf("email tidak valid")
+	}
+	userExists, _ := GetUserFromEmail(insertedDoc.Akun.Email, db)
+	if insertedDoc.Akun.Email == userExists.Email {
+		return fmt.Errorf("email sudah terdaftar")
+	}
+	if strings.Contains(insertedDoc.Akun.Password, " ") {
+		return fmt.Errorf("password tidak boleh mengandung spasi")
+	}
+	if len(insertedDoc.Akun.Password) < 8 {
+		return fmt.Errorf("password terlalu pendek")
+	}
+
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return fmt.Errorf("kesalahan server : salt")
+	}
+	hashedPassword := argon2.IDKey([]byte(insertedDoc.Akun.Password), salt, 1, 64*1024, 4, 32)
+	user := bson.M{
+		"_id":      objectId,
+		"email":    insertedDoc.Akun.Email,
+		"password": hex.EncodeToString(hashedPassword),
+		"salt":     hex.EncodeToString(salt),
+	}
+	pengguna := bson.M{
+		"username": insertedDoc.Username,
+		"akun": model.User{
+			ID: objectId,
+		},
+	}
+	_, err = InsertOneDoc(db, "user", user)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+	_, err = InsertOneDoc(db, "pengguna", pengguna)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+
+	// Generate and send the verification code via WhatsApp
+	code := helper.GetVerificationCode()
+	err = helper.SetVerificationCode(insertedDoc.Akun.PhoneNumber, code)
+	if err != nil {
+		return fmt.Errorf("kesalahan server: %v", err)
+	}
+	err = helper.SendWhatsAppMessage(insertedDoc.Akun.PhoneNumber, code)
+	if err != nil {
+		return fmt.Errorf("gagal mengirim pesan WhatsApp: %v", err)
+	}
+
+	return nil
+}
+
+
 
 //<--- Login --->
 //<--- Login --->
